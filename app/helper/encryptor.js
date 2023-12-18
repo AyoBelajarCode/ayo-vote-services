85ecd3480f0345f887f3d7c45ff29970777f2be1
wtfychxx <fadhliyulyanto@gmail.com>
2023-12-18 19:54:11 +0700

    update: add encryption key and iv to .env

--- app/helper/encryptor.js
+++ app/helper/encryptor.js
@@ -1,8 +1,10 @@
 const crypto = require('crypto')
+const dotenv = require('dotenv')
+dotenv.config()
 
 function sha256Encryptor(action, value) {
-    const key = Buffer.from(crypto.createHash('sha256').update('4y0v0t3k3y').digest('hex'), 'hex') // Kunci dengan panjang 32 byte (256 bit)
-    const iv = Buffer.from(crypto.createHash('md5').update('4y0v0t31v').digest('hex'), 'hex')
+    const key = Buffer.from(crypto.createHash('sha256').update(process.env.ENCRYPTION_KEY).digest('hex'), 'hex') // Kunci dengan panjang 32 byte (256 bit)
+    const iv = Buffer.from(crypto.createHash('md5').update(process.env.ENCRYPTION_IV).digest('hex'), 'hex')
 
     if (action === 'encrypt') {
         let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
